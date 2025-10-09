8a491b74c49c8d33b46a9c95dcbff207f4f5fef1
thivy <thivyakanth@gmail.com>
2025-10-09 16:17:09 +1100

    fix: Refactor error handling in podcastOrchestratorAgent for improved stability

--- api/src/agents/orchestrator-agent.ts
+++ api/src/agents/orchestrator-agent.ts
@@ -15,41 +15,46 @@ const setupStatus = (context: OrchestrationContext, status: PodcastStatus) => {
 export const podcastOrchestratorAgent: OrchestrationHandler = function* (
   context: OrchestrationContext
 ) {
-  setupStatus(context, "INITIALIZING");
-  const input = context.df.getInput<RequestBody>();
+  try {
+    setupStatus(context, "INITIALIZING");
+    const input = context.df.getInput<RequestBody>();
 
-  // only send to content extractor if url or data is provided
-  if (input.url || input.data) {
-    setupStatus(context, "EXTRACTING_INSIGHTS");
-    const insights: AnalyzeResult = yield context.df.callActivity(
-      contentExtractorAgent.name,
+    // only send to content extractor if url or data is provided
+    if (input.url || input.data) {
+      setupStatus(context, "EXTRACTING_INSIGHTS");
+      const insights: AnalyzeResult = yield context.df.callActivity(
+        contentExtractorAgent.name,
+        input
+      );
+
+      input.scriptContent = insights.result?.contents[0].markdown || "";
+    }
+
+    setupStatus(context, "WRITING_SCRIPT");
+    const podcastScript: PodcastScript = yield context.df.callActivity(
+      scriptWriterAgent.name,
       input
     );
 
-    input.scriptContent = insights.result?.contents[0].markdown || "";
+    setupStatus(context, "CONVERTING_SSML");
+    const ssml = yield context.df.callActivity(
+      ssmlConvertAgent.name,
+      podcastScript.script
+    );
+
+    setupStatus(context, "CREATING_AUDIO");
+    const speechInput: SpeechSynthInput = {
+      ssml: ssml,
+    };
+
+    const speech = yield context.df.callActivity(speechAgent.name, speechInput);
+
+    setupStatus(context, "COMPLETED");
+    return { podcastScript, podcastUrl: speech };
+  } catch (err) {
+    setupStatus(context, "FAILED");
+    throw err;
   }
-
-  setupStatus(context, "WRITING_SCRIPT");
-  const podcastScript: PodcastScript = yield context.df.callActivity(
-    scriptWriterAgent.name,
-    input
-  );
-
-  setupStatus(context, "CONVERTING_SSML");
-  const ssml = yield context.df.callActivity(
-    ssmlConvertAgent.name,
-    podcastScript.script
-  );
-
-  setupStatus(context, "CREATING_AUDIO");
-  const speechInput: SpeechSynthInput = {
-    ssml: ssml,
-  };
-
-  const speech = yield context.df.callActivity(speechAgent.name, speechInput);
-
-  setupStatus(context, "COMPLETED");
-  return { podcastScript, podcastUrl: speech };
 };
 
 df.app.orchestration(podcastOrchestratorAgent.name, podcastOrchestratorAgent);
